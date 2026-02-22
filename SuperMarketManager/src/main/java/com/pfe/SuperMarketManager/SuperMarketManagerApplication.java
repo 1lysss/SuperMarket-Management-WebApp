e1ee34f257b52272458381ad6c91353aa5260a24
Ilyass <irrifiilyass@gmail.com>
2026-02-22 15:02:21 +0000

    Let's go!

---
+++ SuperMarketManager/src/main/java/com/pfe/SuperMarketManager/SuperMarketManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pfe.SuperMarketManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMarketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMarketManagerApplication.class, args);
+	}
+
+}
---
+++ SuperMarketManager/src/test/java/com/pfe/SuperMarketManager/SuperMarketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfe.SuperMarketManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
