55f9c65fb91144de6825894a11ba4b112c5c34ac
meryem <meryem.zerrad@etu.uae.ac.ma>
2026-05-04 21:22:04 +0100

    LIST EMPLOYEE CSS JS

--- src/main/java/com/pfe/SuperMarketManager/SuperMarketManagerApplication.java
+++ src/main/java/com/pfe/SuperMarketManager/SuperMarketManagerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SuperMarketManagerApplication {
+public class uperMarketManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SuperMarketManagerApplication.class, args);
