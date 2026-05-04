5650d4cdcceb20539d118d09245b8b643d48eee7
Ilyass <irrifiilyass@gmail.com>
2026-05-04 21:58:35 +0100

    Improved UI for list of employees

--- src/main/java/com/pfe/SuperMarketManager/SuperMarketManagerApplication.java
+++ src/main/java/com/pfe/SuperMarketManager/SuperMarketManagerApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class uperMarketManagerApplication {
+public class SuperMarketManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SuperMarketManagerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SuperMarketManagerApplication.class, args);
+    }
 
 }
--- src/test/java/com/pfe/SuperMarketManager/SuperMarketManagerApplicationTests.java
+++ src/test/java/com/pfe/SuperMarketManager/SuperMarketManagerApplicationTests.java
@@ -1,13 +1,1 @@
-package com.pfe.SuperMarketManager;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SuperMarketManagerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
